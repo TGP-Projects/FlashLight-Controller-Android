14ade4b33b7d8344cbc2e8394c93c880d387ba18
MatheusTGP ツ <89700288+MatheusTGamerPro@users.noreply.github.com>
2022-05-22 23:42:03 -0300

    Adicionar o código-fonte do programa.
    - 2/2 | Concluído

---
+++ Pages/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+using Xamarin.Android;
+using Android;
+using XamarinFormsUiLib;
+
+namespace FlashApp
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			bool flashOn = false;
+			InitializeComponent();
+			
+			btt1.Clicked += delegate{
+				if (flashOn){
+					flashOn = false;
+					Flashlight.TurnOffAsync();
+					btt1.Text = "Flash Desligado";
+					btt1.TextColor = Color.White;
+				} else{
+					flashOn = true;
+					Flashlight.TurnOnAsync();
+					btt1.Text = "Flash Ligado";
+					btt1.TextColor = Color.Orange;
+				}
+			};
+		}
+	}
+}
