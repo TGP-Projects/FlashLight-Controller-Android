375d3b90abbf2388b3909e342517faa6530df352
MatheusTGP ツ <89700288+MatheusTGamerPro@users.noreply.github.com>
2022-05-22 23:39:40 -0300

    Adicionar arquivos do programa.
    - 1/2

---
+++ App.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+using Xamarin.Forms;
+
+namespace FlashApp
+{
+	public partial class App : Application
+	{
+		public App()
+		{
+			InitializeComponent();
+			MainPage = new MainPage();
+		}
+	}
+}
